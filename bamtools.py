2f1a1ee4cc178a1eb64f3202d0e5f6c3b878dbee
Dong pin Oh <dhehdqls@gmail.com>
2017-07-10 14:17:14 -0700

    Delete bamtools.py

--- bamtools.py
+++
@@ -1,7 +0,0 @@
-from subprocess import call
-
-def run(input_bam):
-    commands = ["bamtools", "split", "-in", input_bam, "-reference"]
-    call(commands)
-
-    ### what is the proper command of calling bamtools???
