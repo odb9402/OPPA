8f15fb89f8c70b32fb3abd29aa9f7158f8a50b21
odb9402 <dhehdqls@gmail.com>
2017-07-10 14:50:00 -0700

    delete

--- MOE/python/cpp_wrappers/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Implementations of the ABCs in the :mod:`moe.optimal_learning.python.interfaces` package using calls to the C++ library (GPP.so).
-
-The modules in this package provide hooks into the C++ implementation of optimal_learning's features. There are functions
-and classes for model selection, gaussian process construction, expected improvement optimization, etc.
-
-See the package comments for :mod:`moe.optimal_learning.python.interfaces` for an overview of optimal_learning's capabilities.
-
-"""
