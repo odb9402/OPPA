1de49a72f10578346e0ef2b5b95eb0f56e44f9c7
Dong pin Oh <dhehdqls@gmail.com>
2017-07-19 15:57:52 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
+import os
+import sys
+
 from setuptools import setup
 
-setup(name='OPPA',
-      version='0.1',
-      description='Oppa :: Optimize Parameter in Peak'
-                  ' detection Algorithm by bayesian optimization.',
-      url='http://github.com/odb9402/OPPA',
-      author='dong pin oh',
-      license='MIT',
-      packages=[],
-      install_requires=['scikit-learn','scipy','numpy','Cython','MACS2'],
-      zip_safe=False)
+def main():
+      if float(sys.version[:3])<2.7 or float(sys.version[:3])>=2.8:
+        sys.stderr.write("python version must be 2.7\n")
+        sys.exit(1)
 
+      setup(name='OPPA',
+            version='0.1',
+            description='Oppa :: Optimize Parameter in Peak'
+                        ' detection Algorithm by bayesian optimization.',
+            author='dong pin oh',
+            author_email='dhehdqls@gmail.com',
+            url='http://github.com/odb9402/OPPA',
+            scripts=['oppa.py']
+            author='dong pin oh',
+            license='MIT',
+            packages=[],
+            install_requires=['scikit-learn','scipy','numpy','Cython','MACS2'],
+            zip_safe=False)
+
+if __name__ == '__main__':
+      main()
