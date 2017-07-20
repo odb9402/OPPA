972f076fce8c06b2923669a74f75f56baf8ee4d9
Dong pin Oh <dhehdqls@gmail.com>
2017-07-19 17:04:11 -0700

    Update dependencies.py

--- dependencies/dependencies.py
+++ dependencies/dependencies.py
@@ -27,11 +27,16 @@ def main():
 
 #    cmd = 'sudo mv boost_1_64_0 /usr/local/'
 #    subprocess.call(cmd, shell=True)
+
     robjects.r('''install.packages( 'multicore_0.2.tar.gz' , repos=NULL , dependencies = TRUE)''')
     robjects.r('''install.packages( 'caTools')''')
+    robjects.r('''install.packages( 'bitops')''')
+    robjects.r('''install.packages( 'snow')''')
+    robjects.r('''install.packages( 'snowfall')''')
     robjects.r('''source("https://bioconductor.org/biocLite.R")''')
-    robjects.r('''biocLite("BiocInstaller")''')
-    robjects.r('''install.packages( 'spp.tar.gz' )''')
+    robjects.r('''biocLite("Rsamtools",suppressUpdata=TRUE)''')
+    robjects.r('''install.packages( 'oppa/spp/spp_1.14.tar.gz' )''')
+#    robjects.r('''install.packages( 'spp.tar.gz' )''')
 
 
 
