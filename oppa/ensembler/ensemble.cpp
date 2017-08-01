0aee82829c98ed41fb04ef379527788747d65060
ejinhee <ejjinhee@gmail.com>
2017-07-31 20:31:08 -0700

    All chromosome

--- oppa/ensembler/ensemble.cpp
+++ oppa/ensembler/ensemble.cpp
@@ -80,10 +80,12 @@ ens_main(PyObject *self, PyObject *args){
 
    double smallest_value, pct;
    int cnt=0;
+   int chr_check = 0;
+   char current_chr = it[0]->chr[3];
 
    cout <<it[0]->chr<< " " <<it[1]->chr<<endl;
    smallest_value = it[0]->chr_start;
-   while(it[0]->chr[3] == it[1]->chr[3]){
+   while(1){
       for(int i=0; i <file_num; i++){
          if(abs(smallest_value - it[i]->chr_start) <allowable_difference){
             cnt++;
@@ -94,17 +96,41 @@ ens_main(PyObject *self, PyObject *args){
 
       if(pct > threshhold){
          cout <<"=========="<<file_num << " of " <<cnt<<" files are match"<<"=========="<<endl;
+cout<< it[0]->chr <<endl; 
          cout << it[0]->chr_start<<endl;
          cout << it[1]->chr_start<<endl;
          cout <<"===================="<<endl;
+
       }
       cnt = 0;
       for(int i=0; i <file_num; i++){
-         if(abs(smallest_value - it[i]->chr_start) <allowable_difference){
+         if(abs(smallest_value - it[i]->chr_start) <allowable_difference && current_chr == it[i]->chr[3] ){
             it[i]++;
+            if(it[i]==peak_vec[i].end()){
+               it[i]--;
+               it[i]->chr[3] = 'D';
+            }
          }
       }
+      chr_check=0;
+      for(int i=0; i <file_num; i++){
+         if(it[i]!= peak_vec[i].end() && current_chr == it[i]->chr[3])
+            chr_check++;
+      }
+      if(double(chr_check) / double(file_num) < threshhold){
+cout <<" -----------------=======================================";
+         for(int i=0; i< file_num; i++){
+            if(it[i]->chr[3]=='D')
+                 return Py_BuildValue("i",1);
+            while(it[i]->chr[3] == current_chr){
+               it[i]++;
+            }
+         }
+         current_chr = it[0]->chr[3];
+      }
       smallest_value = it[0]->chr_start < it[1]->chr_start? it[0]->chr_start : it[1]->chr_start;
+
+
    }
    //////// C++ value to Python Object /////////////////
    return Py_BuildValue("i",1);
